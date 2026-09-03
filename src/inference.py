fd0fad02d40ae883fbfab36f75db4bcc59ac799e
Terry <hso37@gatech.edu>
2026-09-03 21:12:06 +0100

    Added ROC Curve, PR Curve

--- src/inference.py
+++ src/inference.py
@@ -4,27 +4,45 @@ from model import CNN
 from data_load import data_load
 from torcheval.metrics import MulticlassAccuracy
 import torch
+from torchmetrics.classification import BinaryROC, BinaryPrecisionRecallCurve, BinaryF1Score
+
 
 train_loader, val_loader, test_loader = data_load()
-metric = MulticlassAccuracy()
+
 device = 'cuda'
 
 
 def inference(saved_model_path,test_loader, model = CNN(num_classes=2)):
+    metric = MulticlassAccuracy().to(device)
+    roc = BinaryROC().to(device)
+    f1 = BinaryF1Score().to(device)
+    pr = BinaryPrecisionRecallCurve().to(device)
+
     model.load_state_dict(torch.load(saved_model_path,weights_only = True))
     model = model.to(device)
+
     model.eval()
 
     with torch.no_grad():
         metric.reset()
+        roc.reset()
+        f1.reset()
+        pr.reset()
+
         for image_batch, labels in test_loader:
             image_batch = image_batch.to(device)
             labels = labels.to(device)
             outputs = model(image_batch)
             preds = outputs.argmax(dim=1)
             metric.update(preds,labels)
+            roc.update(outputs[:,1], labels)
+            pr.update(outputs[:,1], labels)
+            f1.update(outputs[:,1], labels)
     acc = metric.compute()
-    print(f"Test Accuracy: {acc: .2f}")
+    precision, recall, pr_threshold = pr.compute()
+    f1_score = f1.compute()
+    fpr, tpr, roc_threashold = roc.compute()
+    return { "test_acc" :  acc, "roc": (fpr, tpr, roc_threashold), "PR_curve":(precision, recall, pr_threshold), "f1":f1_score}
         
 
 
--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ from torchvision import models
 import torch
 from torcheval.metrics import MulticlassAccuracy
 import copy
-
+from torchmetrics.classification import BinaryROC, BinaryPrecisionRecallCurve, BinaryF1Score
 
 def training_loop(train_loader, val_loader, model = CNN(num_classes=2), lr = 1e-3, num_epoch = 300, patience = 30, factor=0.1):
     device = "cuda"
@@ -17,13 +17,22 @@ def training_loop(train_loader, val_loader, model = CNN(num_classes=2), lr = 1e-
     best_loss = float("inf")
     best_acc = None
     metric = MulticlassAccuracy()
+    roc = BinaryROC()
+    f1 = BinaryF1Score()
+    pr = BinaryPrecisionRecallCurve()
+
+    metric = metric.to(device)
+    roc = roc.to(device)
+    f1 = f1.to(device)
+    pr = pr.to(device)
     val_loss_list = []
     val_acc_list = []
     train_loss_list = []
 
     for epoch in range(num_epoch):
         model.train()
-        metric.reset()
+
+
         epoch_train_loss = 0
         for image_batch, labels in train_loader:
 
@@ -41,6 +50,10 @@ def training_loop(train_loader, val_loader, model = CNN(num_classes=2), lr = 1e-
         epoch_avg_train_loss = epoch_train_loss/len(train_loader)
         
         model.eval()
+        metric.reset()
+        roc.reset()
+        f1.reset()
+        pr.reset()
         with torch.no_grad():
             epoch_val_loss = 0
             
@@ -54,14 +67,22 @@ def training_loop(train_loader, val_loader, model = CNN(num_classes=2), lr = 1e-
                 epoch_val_loss += val_loss.item()
                 preds = outputs.argmax(dim=1)
                 metric.update(preds, labels)
+                roc.update(outputs[:,1], labels)
+                pr.update(outputs[:,1], labels)
+                f1.update(outputs[:,1], labels)
 
             epoch_avg_val_loss = epoch_val_loss/len(val_loader)
             scheduler.step(epoch_avg_val_loss)
-            
             acc = metric.compute()
+            precision, recall, pr_threshold = pr.compute()
+            f1_score = f1.compute()
+            fpr, tpr, roc_threashold = roc.compute()
             if epoch_avg_val_loss < best_loss:
                 best_loss = epoch_avg_val_loss
                 best_acc = acc.item()
+                best_roc = fpr, tpr, roc_threashold
+                best_pr  = precision, recall, pr_threshold
+                best_f1 =  f1_score.item()
                 best_model = copy.deepcopy(model.state_dict())
                 no_improvement_count = 0
                 
@@ -77,22 +98,25 @@ def training_loop(train_loader, val_loader, model = CNN(num_classes=2), lr = 1e-
 
             train_loss_list.append(epoch_avg_train_loss)
             val_loss_list.append(epoch_avg_val_loss)
-            val_acc_list.append(acc)
+            val_acc_list.append(acc.item())
             print(optimizer.param_groups[0]['lr'])
 
             if no_improvement_count == patience:
                 print(f"Best Average Validation Loss: {best_loss:.4f}, Saving Model..")
-                torch.save(best_model, f"../saved_models/mini_vgg_acc_{best_acc*100:.2f}_loss_{best_loss:.4f}_lr_{lr}_epoches_{epoch}_patience_{patience}")
+                torch.save(best_model, f"../saved_models/mini_vgg_acc_{best_acc:.2f}_loss_{best_loss:.4f}_lr_{lr}_epoches_{epoch}_patience_{patience}")
                 print('Model Saved')
                 
-                return train_loss_list, val_loss_list, val_acc_list
+                
+                
+                return {"train_loss": train_loss_list, "val_loss" : val_loss_list, "val_acc" :  val_acc_list, "roc": best_roc, "PR_curve":best_pr, "f1":best_f1}
 
 
     print(f"Best Average Validation Loss: {best_loss:.4f}, Saving Model..")
-    torch.save(best_model, f"../saved_models/mini_vgg_acc_{best_acc*100:.2f}_loss_{best_loss:.4f}_lr_{lr}_epoches_{num_epoch}_patience_{patience}")
+    torch.save(best_model, f"../saved_models/mini_vgg_acc_{best_acc:.2f}_loss_{best_loss:.4f}_lr_{lr}_epoches_{num_epoch}_patience_{patience}")
     print('Model Saved')
 
-    return train_loss_list, val_loss_list, val_acc_list
+
+    return {"train_loss": train_loss_list, "val_loss" : val_loss_list, "validation" :  val_acc_list, "roc": best_roc, "PR_curve":best_pr, "f1":best_f1}
 
 
    
